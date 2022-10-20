000092ddcb01cea5d7e948574737a4642d4c9c9f
salluthdev <salluthdev@gmail.com>
2022-10-20 09:57:07 +0700

    Add hero section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -13,14 +14,71 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.nav_wrapper}>
-        <div className="wrapper">
-          <Image src={"/logo.svg"} width={206} height={40} />
+      <div className={styles.navbar}>
+        <div className={`wrapper ${styles.nav_wrapper}`}>
+          <Link href="/">
+            <Image
+              src={"/svg/logo.svg"}
+              width={206}
+              height={40}
+              alt="Logo Ducky Drop"
+            />
+          </Link>
+          <div className={styles.nav_content_wrapper}>
+            <nav>
+              <p>Twitter</p>
+              <p>Telegram</p>
+              <p>Ducky Paper</p>
+            </nav>
+            <button>DApp</button>
+          </div>
         </div>
       </div>
       <main>
         <div className="wrapper">
-          <h1 className={styles.title}>Massdropping Crypto in a Ducky Way</h1>
+          <div className={styles.hero}>
+            <h1>Massdropping Crypto in a Ducky Way</h1>
+            <div className={styles.taglines_wrapper}>
+              <div className={styles.tagline}>
+                <Image
+                  src={"/svg/icon-tagline-flash.svg"}
+                  width={24}
+                  height={24}
+                  alt="Tagline - Fast"
+                />
+                <p>Fast</p>
+              </div>
+              <Image
+                src={"/svg/icon-tagline-hexagon.svg"}
+                width={22}
+                height={24}
+              />
+              <div className={styles.tagline}>
+                <Image
+                  src={"/svg/icon-tagline-discount.svg"}
+                  width={24}
+                  height={24}
+                  alt="Tagline - Cheap"
+                />
+                <p>Cheap</p>
+              </div>
+              <Image
+                src={"/svg/icon-tagline-hexagon.svg"}
+                width={22}
+                height={24}
+              />
+              <div className={styles.tagline}>
+                <Image
+                  src={"/svg/icon-tagline-ducky.svg"}
+                  width={24}
+                  height={24}
+                  alt="Tagline - Ducky"
+                />
+                <p>Ducky</p>
+              </div>
+            </div>
+            <button>Ducky DApp</button>
+          </div>
         </div>
       </main>
     </div>
