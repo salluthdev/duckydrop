c6003ccb95f56ae843864c568d3ceae6035acf37
salluthdev <salluthdev@gmail.com>
2022-10-28 14:26:15 +0700

    Refactor code

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
